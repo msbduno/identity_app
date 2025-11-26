c208afdb553e62a684fd5d6a4faad15fed426ea1
msbdunno <bossard.mathis@gmail.com>
2025-11-26 14:57:15 +0100

    backend: initial Spring Boot project

---
+++ backend/src/main/java/com/indentity/identity_app/IdentityAppApplication.java
@@ -0,0 +1,13 @@
+package com.indentity.identity_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/indentity/identity_app/IdentityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indentity.identity_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
