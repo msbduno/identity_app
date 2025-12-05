87e74bd36e557c4e292e850ea23dccf9303f7444
msbdunno <bossard.mathis@gmail.com>
2025-12-05 19:08:14 +0100

    feat : user entity

---
+++ backend/src/main/java/com/indentity/identity_app/entity/Role.java
@@ -0,0 +1,6 @@
+package com.indentity.identity_app.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- backend/src/main/java/com/indentity/identity_app/entity/User.java
+++ backend/src/main/java/com/indentity/identity_app/entity/User.java
@@ -20,4 +20,7 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
